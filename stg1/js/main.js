b7d3d5a3f013603d5e5d2d5cdfddc8d4ccdc76c5
NutmegMk0 <nutmegmk0@gmail.com>
2017-09-28 23:11:09 +0900

    れんだー

--- stg1/js/main.js
+++ stg1/js/main.js
@@ -3,3 +3,17 @@ if(!PIXI.utils.isWebGLSupported()){
   type = "canvas"
 }
 PIXI.utils.sayHello(type);
+
+function init () {
+  // 新しいレンダラー作る
+  var renderer = PIXI.autoDetectRenderer(640, 480);
+
+  // それをHTMLファイルに入れる
+  document.body.appendChild(renderer.view);
+
+  // stageという名前の容器（オプジェクト）を作る
+  var stage = new PIXI.Container();
+
+  // レンダラーにstageを受け入れてって伝える
+  renderer.render(stage);
+}
